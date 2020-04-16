49865567679b50f2b0324db33f691f061a3c1a2e
The-Popa <tobias.carlstrom@bredband2.com>
2020-04-16 12:30:16 +0200

    AppTest1

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -9,7 +9,7 @@ public class AppTest {
     @Test
     public void testAdditionPass() {
         // assertEquals(String message, long expected, long actual)
-        assertEquals("error in addition()",  0, App.Calculator.addition(1, 2));
+        assertEquals("error in addition()",  3, App.Calculator.addition(1, 2));
         assertEquals("error in addition()", -3, App.Calculator.addition(-1, -2));
         assertEquals("error in addition()",  9, App.Calculator.addition(9, 0));
 
